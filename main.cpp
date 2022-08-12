821613432418fce90e2d0a849ab6c6594a74e5cb
CursedRock17 <mtglucas1@gmail.com>
2022-08-11 21:48:38 -0400

    Getting Terminal Data

--- main.cpp
+++ main.cpp
@@ -1,12 +1,53 @@
+#include <cstdio>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <vector>
+
 #include "./MenuFolder/Menu.hpp"
 
+std::vector<std::string> packages;
+
+
+void extra(){
+packages.push_back("fmt");
+
+int installed = system("~/vcpkg/vcpkg --version");
+  if(installed != 0){
+            //Begin install of vcpkg
+      std::cout << "Installing vcpkg for you, at your root directory";
+      system("git clone https://github.com/Microsoft/vcpkg.git");
+      system("~/vcpkg/bootstrap-vcpkg.sh");
+    }  
+
+
+    for(auto const& lib : packages){
+      std::string fitted_lib = "~/vcpkg/vcpkg install " + lib;
+      const char* char_string = fitted_lib.c_str();
+      system(char_string);
+
+      std::array<char, 128> buffer;
+      std::string result;
+      std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("~/vcpkg/vcpkg install fmt", "r"), pclose);
+
+      while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+          result += buffer.data();
+      }
+    
+  }
+}
+
 int main(){
-  system("clear");
+  //system("clear");
 
-  std::cout << "Hello and Welcome to Your Password Finder" << std::endl;
+  //std::cout << "Hello and Welcome to Your Password Finder" << std::endl;
 
+  //menuStart();
+  //extra();
 
-  menuStart();
+  std::cout << "Hello";
   return 0;
 }
+
